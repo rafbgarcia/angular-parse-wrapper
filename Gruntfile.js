fdc6a52ff34d88331e4db5aa5a41df9096633fc4
Rafael Garcia <rafbgarcia@gmail.com>
2014-05-16 14:02:46 -0300

    Configure for distribution

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,10 +4,30 @@ module.exports = function(grunt) {
       unit: {
         configFile: 'karma.conf.js'
       }
-    }
+    },
+
+    uglify: {
+      options: {
+        mangle: false,
+        compress: true,
+        beautify: false
+      },
+      js: {
+        options: {
+          separator: '\n;'
+        },
+        files: {
+          'dist/angular-parse-wrapper.min.js': [
+            'src/parse-wrapper.js'
+          ]
+        }
+      }
+    },
+
   });
 
   grunt.loadNpmTasks('grunt-karma');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
   grunt.registerTask('test_unit', ['karma']);
 };
