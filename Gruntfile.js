1f9ca15e481e00ce9e8afb07c4c64bf63faf8bed
Rafael Garcia <rafbgarcia@gmail.com>
2014-05-25 15:47:42 -0300

    Adds task dist

--- Gruntfile.js
+++ Gruntfile.js
@@ -33,6 +33,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-karma');
   grunt.loadNpmTasks('grunt-contrib-uglify');
 
+  grunt.registerTask('dist', ['uglify']);
   grunt.registerTask('test_unit', ['karma']);
   grunt.registerTask('watch_test_unit', ['karma:watch']);
 };
