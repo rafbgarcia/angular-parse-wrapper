555354d842db3378de867912b8cc9ccd324121a1
Rafael Garcia <rafbgarcia@gmail.com>
2014-05-25 15:13:00 -0300

    Refactors tests

--- test/parse-wrapper.js
+++ test/parse-wrapper.js
@@ -1,18 +1,9 @@
 describe('Wrap Parse', function() {
-  var $rootScope, $compile, $timeout, Product, p, element, wrapParse;
-
-  function flush() {
-    try {
-      $timeout.flush();
-    } catch(e) {}
-  }
+  var Product, wrapParse;
 
   beforeEach(module('wrapParse'));
 
-  beforeEach(inject(function(_$rootScope_, _$compile_, _$timeout_, _wrapParse_) {
-    $compile = _$compile_;
-    $rootScope = _$rootScope_;
-    $timeout = _$timeout_;
+  beforeEach(inject(function(_wrapParse_) {
     wrapParse = _wrapParse_;
 
     Company = wrapParse('Company', {
@@ -32,13 +23,10 @@ describe('Wrap Parse', function() {
     product = new Product({
       price: 10.5,
       weight: 200,
-      inStock: true
+      inStock: false
     });
   }));
 
-  it("is defined", function() {
-    expect(wrapParse).toBeDefined();
-  });
 
   it('has attribute _name equal to Relation', function() {
     expect(Product._name).toBe('Relation');
@@ -75,22 +63,28 @@ describe('Wrap Parse', function() {
 
       expect(product.inStock).toBe(true);
     });
+
+    it('accepts default value', function() {
+      expect(product.available).toEqual(true);
+    });
   });
 
-  it('accepts default value', function() {
-    expect(product.available).toEqual(true);
-  });
+  describe('Angular form integration', function() {
+    var $rootScope, $compile, element;
 
-  describe('Parse.Object', function() {
     function changeInputVal(val) {
       element.val(val).trigger('input');
     }
 
-    beforeEach(function() {
-      element = $compile('<input ng-model="product.price" type="text">')($rootScope);
+    beforeEach(inject(function(_$rootScope_, _$compile_) {
+      $compile = _$compile_;
+      $rootScope = _$rootScope_;
+
       $rootScope.product = product;
-      $rootScope.$digest();
-    });
+      $rootScope.$apply(function () {
+        element = $compile('<input ng-model="product.price" type="text">')($rootScope);
+      });
+    }));
 
     it('shows de input with correct values', function() {
       expect(element.val()).toBe('10.5');
@@ -109,139 +103,10 @@ describe('Wrap Parse', function() {
 
       expect($rootScope.product.price).toBe('15');
     });
+  });
 
-    describe('parses the property to its type: ', function() {
-      describe('Number', function() {
-        it('converts String to Int', function() {
-          product.price = '10';
-
-          product.parseFields();
-
-          expect(product.price).toBe(10);
-        });
-
-        it('keeps undefined as undefined', function() {
-          product.price = undefined;
-
-          product.parseFields();
-
-          expect(product.price).toBe(undefined);
-        });
-      });
-
-      describe('Boolean', function() {
-        it('converts numeric String different from "0" to true', function() {
-          product.inStock = '1';
-
-          product.parseFields();
-
-          expect(product.inStock).toBe(true);
-        });
-
-        it('converts numeric String "0" to false', function() {
-          product.inStock = '0';
-
-          product.parseFields();
-
-          expect(product.inStock).toBe(false);
-        });
-
-        it('converts non-zero numbers to true', function() {
-          product.inStock = 1;
-
-          product.parseFields();
-
-          expect(product.inStock).toBe(true);
-        });
-
-        it('converts 0 to false', function() {
-          product.inStock = 0;
-
-          product.parseFields();
-
-          expect(product.inStock).toBe(false);
-        });
-
-        it('keeps Boolean value', function() {
-          product.inStock = true;
-
-          product.parseFields();
-
-          expect(product.inStock).toBe(true);
-        });
-
-        it('keeps undefined as undefined', function() {
-          product.inStock = undefined;
-
-          product.parseFields();
-
-          expect(product.inStock).toBe(undefined);
-        });
-      });
-
-      describe('Date', function() {
-        var date;
-        beforeEach(function() {
-          date = new Date(2014, 02, 03, 0, 0, 0);
-        });
-
-        it('converts String to Date according to the dateFormat', function() {
-          product.createDate = '2014-03-03';
-
-          product.parseFields();
-
-          expect(product.createDate.getTime()).toBe(date.getTime());
-        });
-
-        it('keeps Date value, setting time to 00:00:00', function() {
-          product.createDate = new Date('2014-03-03');
-
-          product.parseFields();
-
-          expect(product.createDate.getTime()).toBe(date.getTime());
-        });
-
-        it('keeps undefined value', function() {
-          product.createDate = undefined;
-
-          product.parseFields();
-
-          expect(product.createDate).toBe(undefined);
-        });
-
-        it('changes dateFormat', function() {
-          wrapParse.dateFormat = 'DD/MM/YYYY';
-          product.createDate = new Date('03/03/2014');
-
-          product.parseFields();
-
-          expect(product.createDate.getTime()).toBe(date.getTime());
-        });
-      });
-
-      describe('Relations', function() {
-        var companyId = '12Xca12d';
-
-        it('create a new Relation for a given id', function() {
-          product.company = companyId;
-
-          product.parseFields();
-
-          expect(product.company instanceof Company).toBeTruthy();
-          expect(product.company.id).toBe(companyId);
-        });
-        it('keeps the Relation value', function() {
-          product.company = new Company({id: companyId})
-
-          product.parseFields();
-
-          expect(product.company.id).toBe(companyId);
-        })
-      });
-    });
-
+  describe('Parse.Object', function() {
     describe('#save', function() {
-      var value = '';
       beforeEach(function() {
         spyOn(product, 'parseFields');
       });
@@ -270,6 +135,137 @@ describe('Wrap Parse', function() {
 
         expect(Parse.Query.prototype.get).toHaveBeenCalledWith(id, undefined, undefined);
       });
+    });
+  });
+
+  describe('Conversions', function() {
+    describe('Number', function() {
+      it('converts String to Int', function() {
+        product.price = '10';
+
+        product.parseFields();
+
+        expect(product.price).toBe(10);
+      });
+
+      it('keeps undefined as undefined', function() {
+        product.price = undefined;
+
+        product.parseFields();
+
+        expect(product.price).toBe(undefined);
+      });
+    });
+
+    describe('Boolean', function() {
+      it('converts numeric String different from "0" to true', function() {
+        product.inStock = '1';
+
+        product.parseFields();
+
+        expect(product.inStock).toBe(true);
+      });
+
+      it('converts numeric String "0" to false', function() {
+        product.inStock = '0';
+
+        product.parseFields();
+
+        expect(product.inStock).toBe(false);
+      });
+
+      it('converts non-zero numbers to true', function() {
+        product.inStock = 1;
+
+        product.parseFields();
+
+        expect(product.inStock).toBe(true);
+      });
+
+      it('converts 0 to false', function() {
+        product.inStock = 0;
+
+        product.parseFields();
+
+        expect(product.inStock).toBe(false);
+      });
+
+      it('keeps Boolean value', function() {
+        product.inStock = true;
+
+        product.parseFields();
+
+        expect(product.inStock).toBe(true);
+      });
+
+      it('keeps undefined as undefined', function() {
+        product.inStock = undefined;
+
+        product.parseFields();
+
+        expect(product.inStock).toBe(undefined);
+      });
+    });
+
+    describe('Date', function() {
+      var date;
+      beforeEach(function() {
+        date = new Date(2014, 02, 03, 0, 0, 0);
+      });
+
+      it('converts String to Date according to the dateFormat', function() {
+        product.createDate = '2014-03-03';
+
+        product.parseFields();
+
+        expect(product.createDate.getTime()).toBe(date.getTime());
+      });
+
+      it('keeps Date value, setting time to 00:00:00', function() {
+        product.createDate = new Date('2014-03-03');
+
+        product.parseFields();
+
+        expect(product.createDate.getTime()).toBe(date.getTime());
+      });
+
+      it('keeps undefined value', function() {
+        product.createDate = undefined;
+
+        product.parseFields();
+
+        expect(product.createDate).toBe(undefined);
+      });
+
+      it('changes dateFormat', function() {
+        wrapParse.dateFormat = 'DD/MM/YYYY';
+        product.createDate = new Date('03/03/2014');
+
+        product.parseFields();
+
+        expect(product.createDate.getTime()).toBe(date.getTime());
+      });
+    });
+
+    describe('Relations', function() {
+      var companyId = '12Xca12d';
+
+      it('create a new Relation for a given id', function() {
+        product.company = companyId;
+
+        product.parseFields();
+
+        expect(product.company instanceof Company).toBeTruthy();
+        expect(product.company.id).toBe(companyId);
+      });
+
+      it('keeps the Relation value', function() {
+        product.company = new Company({id: companyId})
+
+        product.parseFields();
+
+        expect(product.company.id).toBe(companyId);
+      })
     });
   });
 
